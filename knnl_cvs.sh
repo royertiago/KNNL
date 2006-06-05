1ed32cd7f5874731129be1797f1ee576cdee15e7
habdankw <habdankw@69255271-3e15-0410-bb7f-e34817a9f18a>
2006-06-05 16:05:10 +0000

    Preparation to release.

--- knnl_cvs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cvs -z3 -d:ext:habdankw@knnl.cvs.sourceforge.net:/cvsroot/knnl commit Doxyfile README gcccpuopt.sh Makefile plots_octave.m plots_matlab.m
-cvs -z3 -d:ext:habdankw@knnl.cvs.sourceforge.net:/cvsroot/knnl commit *.cpp
-cvs -z3 -d:ext:habdankw@knnl.cvs.sourceforge.net:/cvsroot/knnl commit *.hpp
-
